c75766c7a516b3be19ba76d92d85abd24f98a346
jesseli02 <lijesse01@gmail.com>
2018-12-26 21:42:52 -0500

    Update and rename mais202.py to code.py

--- code.py
+++ code.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import seaborn as sns
 
-df = pd.read_csv(r'C:\Users\lijes\Projects\Python\Datasets\interest_rates\data.csv')
+#read csv file
+df = pd.read_csv('jesseli02/data.csv')
 
+#create new dataframe with mean of each purpose
 rates = df.groupby(df['purpose']).int_rate.mean()
 rates = rates.reset_index()
 
+#plot bar graph
 ax = sns.barplot(x = 'purpose', y = 'int_rate', data = rates)
 ax.set_xticklabels(ax.get_xticklabels(), rotation = 90)
 
+#print dataframe table
 print(rates.to_string(index = False))
 
